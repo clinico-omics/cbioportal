80a47612e0774f166cef556ce176228e2a69db8a
YJC <yjcyxky@163.com>
2021-05-19 22:25:01 +0800

    Improve docker-compose.

--- docker-compose/setenv.sh
+++ docker-compose/setenv.sh
@@ -1,3 +1,3 @@
 export PORTAL_HOME=/cbioportal/
-export CATALINA_OPTS='-Dauthenticate=false'
-export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxPermSize=1024m -Xms2g -Xmx20g"
+export CATALINA_OPTS='-Dauthenticate=false -Ddbconnector=jndi -Dpersistence.cache_type=no-cache'
+export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxPermSize=2048m -Xms2g -Xmx20g"
